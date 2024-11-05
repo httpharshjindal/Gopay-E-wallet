be05ed993054fbccafd40e5f8b6ae8e1b635c580
Harsh Jindal <httpharshjindal@gmail.com>
2024-11-06 01:37:53 +0530

    afa

--- apps/user-app/components/AppbarClient.tsx
+++ apps/user-app/components/AppbarClient.tsx
@@ -7,22 +7,20 @@ export const AppbarClient = () => {
   const session = useSession();
   const router = useRouter();
 
-  if (session) {
-    return (
-      <div className="w-full">
-        {/* <h1>{JSON.stringify(session)}</h1> */}
-        <Appbar
-          user={session.data?.user}
-          onSignin={signIn}
-          onSignout={async () => {
-            await signOut();
-            window.location.href = "auth/signin";
-          }}
-          onProfile={() => {
-            router.push("/profile");
-          }}
-        />
-      </div>
-    );
-  }
+  return (
+    <div className="w-full">
+      {/* <h1>{JSON.stringify(session)}</h1> */}
+      <Appbar
+        user={session.data?.user}
+        onSignin={signIn}
+        onSignout={async () => {
+          await signOut();
+          window.location.href = "auth/signin";
+        }}
+        onProfile={() => {
+          router.push("/profile");
+        }}
+      />
+    </div>
+  );
 };
