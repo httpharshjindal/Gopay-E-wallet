a1d4aeebb0a0520d2ddb10e1dcd4848f483ba83b
Harsh Jindal <httpharshjindal@gmail.com>
2024-11-05 23:49:05 +0530

    afd:

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,13 +1,12 @@
 import { getServerSession } from "next-auth";
-import { redirect } from 'next/navigation'
+import { redirect } from "next/navigation";
 import { authOptions } from "./lib/authOptions";
 
 export default async function Page() {
   const session = await getServerSession(authOptions);
   if (session?.user) {
-    redirect('/dashboard')
+    redirect("/dashboard");
   } else {
-    redirect('http://localhost:3000/api/auth/signin')
+    redirect(`${process.env.NEXTAUTH_URL}/api/auth/signin`);
   }
-  
 }
