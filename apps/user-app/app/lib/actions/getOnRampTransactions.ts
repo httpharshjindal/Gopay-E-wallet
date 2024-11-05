88c4bda9c8e6bc473b92b68dcc7e6097de626b92
Harsh Jindal <118842011+httpharshjindal@users.noreply.github.com>
2024-11-05 14:21:58 +0530

    Update getOnRampTransactions.ts for vercel

--- apps/user-app/app/lib/actions/getOnRampTransactions.ts
+++ apps/user-app/app/lib/actions/getOnRampTransactions.ts
@@ -4,19 +4,29 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "../authOptions";
 import prisma from "@repo/db/prisma";
 
-export async function getOnRampTransactions() {
-    const session = await getServerSession(authOptions);
-    const txns = await prisma.onRampTransaction.findMany({
-      where: {
-        userId: Number(session?.user?.id),
-      },
-    });
-    return txns.map((t) => ({
-      time: t.startTime,
-      amount: t.amount,
-      status: t.status,
-      provider: t.provider,
-    }));
+// Define the type based on the fields in your 'onRampTransaction' table
+type OnRampTransaction = {
+  startTime: Date;
+  amount: number;
+  status: string;
+  provider: string;
+};
 
-  }
+export async function getOnRampTransactions() {
+  const session = await getServerSession(authOptions);
+  
+  const txns = await prisma.onRampTransaction.findMany({
+    where: {
+      userId: Number(session?.user?.id),
+    },
+  });
+
+  return txns.map((t: OnRampTransaction) => ({
+    time: t.startTime,
+    amount: t.amount,
+    status: t.status,
+    provider: t.provider,
+  }));
+}
+
   
